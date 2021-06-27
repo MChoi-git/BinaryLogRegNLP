62ffbabf739649d92e741ff319b72e7537b2c765
Matt Choi <mattchoi427@gmail.com>
2021-06-27 18:53:44 -0400

    Testing completed
    Just need to add some extra non-critical functions, and clean up.

--- lr.py
+++ lr.py
@@ -2,6 +2,7 @@ import numpy as np
 import sys
 import math
 import pandas as pd
+import os
 
 '''
 Description: 
@@ -12,7 +13,7 @@ Notes:
 
 #Model class containing parameters and learning rate
 #   Honestly I might not even need this, but w/e I'll re-evaluate later
-class binomial_logistic_regression_model:
+class binomialLogisticRegressionModel:
     def __init__(self, number_of_features, initial_learning_rate):
         self.model_parameters = np.zeros(number_of_features,dtype=float)    #Initializes parameter vector [0:M] to all zeros
         self.learning_rate = initial_learning_rate  #Sets the learning rate
@@ -37,12 +38,11 @@ def init_example(ith_example, num_attributes):
     split_ith_example = ith_example.strip().split()     #Strip whitespaces and turn into list of words
     label = split_ith_example[0]                        #Extract and save the label
     feature_vector = np.array(split_ith_example[1:])    #Extract and save the feature vector
-    updated_feature_vector = np.zeros(num_attributes, dtype=float)
+    updated_feature_vector = np.zeros(num_attributes)
     for entry in feature_vector:                        #Convert the feature vector such that x_m = {0,1}, where m belongs to M
         pair = entry.split(":")
         index = int(pair[0])
-        value = int(pair[1])
-        updated_feature_vector[index] = value
+        updated_feature_vector[index] = 1
     return [label, updated_feature_vector]
 
 
@@ -55,7 +55,9 @@ def step_sgd(model, features_vector, real_label):
     eta = model.learning_rate
     x_i = features_vector
     y_i = int(real_label)
-    updated_theta_j = theta_j + (eta * x_i) * (y_i - (math.exp(np.dot(theta_j.T, x_i)))/(1 + (math.exp(np.dot(theta_j.T, x_i)))))
+    numerator = math.exp(np.dot(theta_j.T, x_i))
+    denominator = math.exp(np.dot(theta_j.T, x_i)) + 1
+    updated_theta_j = theta_j + (eta * x_i) * (y_i - (numerator / denominator))
     return updated_theta_j
 
 
@@ -70,8 +72,9 @@ def train_model(model, tsv_dataset_filename, epochs, num_attributes):
     dataset = [init_example(line, num_attributes) for line in data]     #Shuffle this to get a random example instead of current in-order implementation
     #Do a number of iterations of SGD
     for i in range(epochs):
-        model.model_parameters = step_sgd(model, dataset[i][1], dataset[i][0])   #Change i to the shuffled variable
-        print(f"Epoch: {i}")
+        for j in range(len(dataset)):
+            model.model_parameters = step_sgd(model, dataset[j][1], dataset[j][0])   #Change i to the shuffled variable
+        print(f"Epoch: {i} has been completed.")
     return True
 
 
@@ -79,7 +82,7 @@ def train_model(model, tsv_dataset_filename, epochs, num_attributes):
 def calculate_prediction(model, data):
     theta_j = model.model_parameters
     x_i = data
-    probability = 1 / (1 + (math.exp(-np.dot(theta_j.T, x_i))))
+    probability = 1 / (math.exp(-np.dot(theta_j.T, x_i)) + 1)
     if probability >= 0.5:
         return 1
     elif probability < 0.5:
@@ -96,20 +99,15 @@ def predict(model, dataset, num_attributes):
         data = f.readlines()
     #Initialize the examples, separating labels from attributes
     dataset = [init_example(line, num_attributes) for line in data]
-    labels = []
+    predicted_labels = []
+    num_incorrect_labels = 0
     for entry in dataset:
-        labels.append(calculate_prediction(model, entry))
-    return labels
-
-
-#Compare the labels of two datasets and compute the error
-def get_error(labels):
-    return True
-
-
-#Writes the labels to their respective label files
-def write_labels(labels):
-    return True
+        label_value = calculate_prediction(model, entry[1])
+        if entry[0] != str(label_value):
+            num_incorrect_labels += 1
+        predicted_labels.append(label_value)
+    error = num_incorrect_labels / len(dataset)
+    return [error, predicted_labels]
 
 
 #Command line string
@@ -118,14 +116,15 @@ def write_labels(labels):
 
 #Inputs
 formatted_train_input = f"output/largeoutput/{sys.argv[1]}"
-formatted_validation_input = f"output/largeoutput/{sys.argv[2]}"
+formatted_test_input = f"output/largeoutput/{sys.argv[2]}"
 dict_input = f"handout/{sys.argv[3]}"
+train_out = f"output/largeoutput/{sys.argv[4]}"
+test_out = f"output/largeoutput/{sys.argv[5]}"
 '''
 formatted_test_input = f"output/largeoutput/{sys.argv[3]}"
 inputFilenames = [formatted_train_input, formatted_validation_input, formatted_test_input, dict_input]
 #Outputs
-train_out = f"output/largeoutput/{sys.argv[5]}"
-test_out = f"output/largeoutput/{sys.argv[6]}"
+
 metrics_out = f"output/largeoutput/{sys.argv[7]}"
 outputFilenames = [train_out, test_out, metrics_out]
 #Number of epochs (ie. SGD iterations)
@@ -136,16 +135,24 @@ numEpoch = sys.argv[8]'''
 dict_txt = init_dict(dict_input)    #Create a usable dictionary
 _M = len(dict_txt)  #Create constant variable M, where M = # features
 #Initialize model object
-blr_model = binomial_logistic_regression_model(_M, 0.1)
+blr_model = binomialLogisticRegressionModel(_M, 0.1)
 #Train the model
-train_model(blr_model, formatted_train_input, 30, _M)
-#Predict with the trained model
-predict(blr_model, formatted_validation_input, _M)
-#Measure error
-
+train_model(blr_model, formatted_train_input, 60, _M)
+#Predict with the trained model and display error
+train_pair = predict(blr_model, formatted_train_input, _M)
+print(f"Train error is: {train_pair[0]}")
+with open(train_out, "w") as f:
+    for label in train_pair[1]:
+        f.write(f"{label}\n")
+test_pair = predict(blr_model, formatted_test_input, _M)
+print(f"Test error is: {test_pair[0]}")
+with open(test_out, "w") as f:
+    for label in test_pair[1]:
+        f.write(f"{label}\n")
 
 #Sending model parameters to .csv
 df = pd.DataFrame(blr_model.model_parameters)
+os.remove("model_parameters.csv")
 df.to_csv("model_parameters.csv", header=None, index=None, sep=',', mode='a')
 
 
